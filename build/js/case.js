41409954f7d96fcc7b87d003140593ae7a5d10ed
unknown <ashestihin@ts.media>
2024-05-28 16:48:52 +0300

    added AlfaBank page

--- build/js/case.js
+++ build/js/case.js
@@ -1,18 +1,40 @@
-const videos=document.querySelectorAll('.case__video')
-videos.forEach(el => {
+function hasAudio (video) {
+   return video.mozHasAudio ||
+   Boolean(video.webkitAudioDecodedByteCount) ||
+   Boolean(video.audioTracks && video.audioTracks.length);
+}
+
+function addControlAudio(videos){
+
+   videos.forEach(el => {
+      const video=el.getElementsByTagName('video')
+      
+      if(video.length && hasAudio(video[0])){
    
-   if(el.getElementsByTagName('video').length){
-      el.innerHTML=[el.innerHTML, '<div class="case__video-management case__video-management_mute"></div>']
-      el.querySelector('.case__video-management').addEventListener('click', ()=> {
-         el.querySelector('video').muted=!el.querySelector('video').muted
-         el.querySelector('video').muted?(
-            el.querySelector('.case__video-management').classList.remove('case__video-management_sound'),
-            el.querySelector('.case__video-management').classList.add('case__video-management_mute')
-         ):(
-            el.querySelector('.case__video-management').classList.remove('case__video-management_mute'),
-            el.querySelector('.case__video-management').classList.add('case__video-management_sound')
-         )
-         
-      })
-   }
-});
+         el.innerHTML=[el.innerHTML, '<div class="case__video-management case__video-management_mute"></div>']
+         el.querySelector('.case__video-management').addEventListener('click', ()=> {
+            el.querySelector('video').muted=!el.querySelector('video').muted
+            el.querySelector('video').muted?(
+               el.querySelector('.case__video-management').classList.remove('case__video-management_sound'),
+               el.querySelector('.case__video-management').classList.add('case__video-management_mute')
+            ):(
+               el.querySelector('.case__video-management').classList.remove('case__video-management_mute'),
+               el.querySelector('.case__video-management').classList.add('case__video-management_sound')
+            )
+            
+         })
+      }
+   });
+
+}
+
+const videos=document.querySelectorAll('.case__video')
+
+setTimeout(()=>addControlAudio(videos), 4000)
+
+
+/* function hasAudio (video) {
+   return video.mozHasAudio ||
+   Boolean(video.webkitAudioDecodedByteCount) ||
+   Boolean(video.audioTracks && video.audioTracks.length);
+} */
